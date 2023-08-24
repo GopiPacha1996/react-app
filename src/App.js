607ad0ef2b835482f601b25b43ecde1969675aae
Ubuntu <ubuntu@demo.3dkbsl5sxaqebnqwpzryqvsimb.tx.internal.cloudapp.net>
2023-08-24 02:36:08 +0000

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         *** Hello All Welcome to EMeralds Tech *****
+         Hello All Welcome to EmeraldsTech.  A new way of Learning
         </p>
         <a
           className="App-link"
